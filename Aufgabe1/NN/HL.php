97077c979531527d22fd68912f345395bf7a4357
Laura Zaworski <laurazaworski@Lauras-Air.fritz.box>
2018-10-30 11:43:48 +0100

    Aufgabe1

---
+++ Aufgabe1/NN/HL.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title> Aufgabe 1 </title>
+</head>
+<body>
+
+	<h1>Teilnehmer_innen WebTech</h1>
+	
+	<br/>
+	
+	<img src=./image/fiw.jpg style=“width:53px; height=48px“;/">
+	
+	<br/>
+	
+	<nav>
+	
+	<a href="../aufgabe1.php"> ALLE </a> <a href="./AG.php"> A-G </a> <a href="./HL.php"> H-L </a> <a href="./mr.php"> M-R </a><a href="./SZ.php"> S-Z </a>
+	
+	</nav>
+	
+	<br/>
+	
+	<section>
+	<h2>Alle Teilnehmer_innen</h2>
+	<table>
+	<tr>
+					<th>Vorname</th>
+					<th>Nachname</th>
+					<th>E-Mail-Adresse</th>
+					<th>IP-Adresse</th>
+				</tr>
+				
+				<tr>
+					<td>Steven</td>
+					<td>Hamilton</td>
+					<td>shamiltonu@state.tx.us</td>
+					<td>38.194.91.201</td>
+				</tr>
+				<tr>
+					<td>Virginia</td>
+					<td>Hawkins</td>
+					<td>vhawkinsf@ehow.com</td>
+					<td>93.120.46.203</td>
+				</tr>
+				<tr>
+					<td>Edward</td>
+					<td>Hicks</td>
+					<td>ehicksc@pcworld.com</td>
+					<td>199.153.27.1</td>
+				</tr>
+				<tr>
+					<td>Mark</td>
+					<td>Johnson</td>
+					<td>mjohnsonj@hostgator.com</td>
+					<td>73.87.135.206</td>
+				</tr>
+				<tr>
+					<td>Ruth</td>
+					<td>Jordan</td>
+					<td>rjordan1a@smugmug.com</td>
+					<td>193.140.80.64</td>
+				</tr>
+				<tr>
+					<td>Antonio</td>
+					<td>Kim</td>
+					<td>akim4@odnoklassniki.ru</td>
+					<td>168.244.191.78</td>
+				</tr>
+
+	</table>
+
+	</section>
+	
+</body>
+
+</html>
